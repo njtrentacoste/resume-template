85469929002f3c198f667e664fcaa8466e7ce347
Nicholas J Trentacoste <njtrentacoste@gmail.com>
2015-04-30 12:13:54 -0400

    Inital Commit of code

---
+++ js/ResumeController.js
@@ -0,0 +1,25 @@
+﻿(function () {
+	'use strict';
+	
+	angular
+		.module('resume')
+		.controller('ResumeController', ResumeController);
+
+	ResumeController.$inject = ['ResumeService'];
+
+	function ResumeController(ResumeService) {
+		var model = this;
+
+		(function load() {
+			ResumeService.getResume().then(success, error);
+		})();
+
+		function success(data) {
+			model.resume = data;
+		}
+
+		function error(errorMessage) {
+			console.warn(errorMessage);
+		}
+	}
+})();
---
+++ js/ResumeService.js
@@ -0,0 +1,35 @@
+﻿(function () {
+	'use strict';
+
+	angular
+		.module('resume')
+		.factory('ResumeService', ResumeService);
+
+	ResumeService.$inject = ['$http'];
+
+	function ResumeService($http) {
+		var service = {
+				getResume: getResumeData
+			};
+
+		return service;
+
+		function getResumeData() {
+			var request = $http.get('data/resume.json');
+
+			return request.then(handleSuccess, handleError);
+		}
+
+		function handleSuccess(response) {
+			return response.data;
+		}
+
+		function handleError(response) {
+			if (!angular.isObject(response.data) || !response.data.message) {
+				return ($q.reject("An unknown error occurred."));
+			}
+
+			return ($q.reject(response.data.message));
+		}
+	}
+})();
